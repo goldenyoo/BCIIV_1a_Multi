7c451c1e506ea745de5b0c37989176466cb387f8
goldenyoo <goldenyoo@kaist.ac.kr>
2021-03-24 17:14:12 +0900

    .

--- myClassifier.m
+++ myClassifier.m
@@ -1,6 +1,6 @@
 function [check, result] = myClassifier(fp,Mr,Ml,Qr,Ql,k)
     
-      
+
     check = (0.5*(fp - Mr)'*pinv(Qr)*(fp - Mr) + 0.5*log(det(Qr))) - (0.5*(fp - Ml)'*pinv(Ql)*(fp - Ml) + 0.5*log(det(Ql))); 
  
     if check > 0
---
+++ myClassifier2.m
@@ -0,0 +1,39 @@
+function [check, result] = myClassifier(fp,M1,M2,Q1,Q2,k)
+    
+Sb = (M1-M2)*(M1-M2)';
+Sw = Q1+Q2;
+tmp = pinv(Sb)*Sw;
+
+[V, D] = eig(tmp);
+[d, ind] = sort(abs(diag(D)),'descend');
+D_new = diag(d);
+V_new = V(:,ind);
+
+new_data = V_new'*fp;
+data1 = V_new'*M1;
+data2 = V_new'*M2;
+
+tmp1 = data1 - new_data;
+tmp2 = data2 - new_data;
+
+if sum(tmp1.*tmp1) < sum(tmp2.*tmp2)
+    if k==1
+        result = 0;
+    elseif k==2
+        result = 0;
+    else
+        result = -1;
+    end
+else
+    if k==1
+        result = -1;
+    elseif k==2
+        result = 1;
+    else
+        result = 1;
+    end
+end
+
+check = abs((min(abs(tmp1))) - (min(abs(tmp2))));
+
+end
